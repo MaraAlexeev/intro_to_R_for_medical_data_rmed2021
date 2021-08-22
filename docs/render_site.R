5f4be98fbb06817d0abaf78dcece059fda374112
Mara Alexeev <mara.alexeev@gmail.com>
2021-08-22 01:09:22 -0400

    changed render site function
    to make rendered file live in docs to be the basis of the github page

--- docs/render_site.R
+++ docs/render_site.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
 library(here)
 
-rmarkdown::render(here("Intro_to_R_for_medical_data_workshop.Rmd"), 
-                  output_file = here("data/index.html"))
+rmarkdown::render(here("teaching_doc.Rmd"), 
+                  output_file = here("docs/index.html"))
